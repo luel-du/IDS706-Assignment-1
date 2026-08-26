f9ec47439ae344a43286c31c5f766b293f066813
luel-du <selamtumelsan@gmail.com>
2026-08-26 14:39:27 -0400

    update test file

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,4 @@
 from src.main import welcome_message
 
 def test_welcome_message():
-    assert welcome_message("Ammy") == "Ammy, welcome to the Data Engineering course."
+    assert welcome_message("Luel") == "Luel, welcome to the Data Engineering course."
